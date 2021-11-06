09efc611e2b1804e308105f5dba0981ae99cc58b
Александр Бондаренко <bondarenko_al@rozetka.com.ua>
2021-11-06 12:37:28 +0200

    Add background cover

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -67,7 +67,7 @@
     </div>
 </header>
 
-<section class="u-align-center u-clearfix u-gradient u-section-1" src="" id="sec-9073">
+<section class="u-align-center u-clearfix u-section-1" id="sec-9073">
     <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-text u-text-body-alt-color u-text-default u-title u-text-1">CONSULTING UBC</h1>
         <p class="u-large-text u-text u-text-body-alt-color u-text-default u-text-variant u-text-2"> Стратегия решений юридических вопросов и миграционных процессов</p>
@@ -346,6 +346,11 @@
         <p class="u-align-center-lg u-align-center-md u-align-center-xl u-text u-text-1"> ©&nbsp;ООО "Консалтинг ЮБС" 2005 - 2022</p>
     </div>
 </footer>
+
+<span style="height: 64px; width: 64px; margin-left: 0px; margin-right: auto; margin-top: 0px; background-image: none; right: 20px; bottom: 20px" class="u-back-to-top u-icon u-icon-circle u-opacity u-opacity-85 u-palette-1-base u-spacing-15" data-href="#">
+    <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 551.13 551.13"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d98"></use></svg>
+    <svg class="u-svg-content" enable-background="new 0 0 551.13 551.13" viewBox="0 0 551.13 551.13" xmlns="http://www.w3.org/2000/svg" id="svg-1d98"><path d="m275.565 189.451 223.897 223.897h51.668l-275.565-275.565-275.565 275.565h51.668z"></path></svg>
+</span>
 
 </body>
 </html>
